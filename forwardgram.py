18f8f135b3d387d4898cbfba490476b236e2831d
Yegor.Gorbatov <gorbatov.eg@gmail.com>
2021-12-10 01:49:08 +0300

    Logging impl; error handling improvements.

--- forwardgram.py
+++ forwardgram.py
@@ -1,8 +1,14 @@
 import json
+import logging
 import sys
+from logging import Logger
 
 from telethon import TelegramClient
 from telethon import events
+
+# Logging
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(module)s: %(message)s', filename='logs/forwardgram.log', filemode='a', level=logging.INFO)
+LOGGER = logging.getLogger('ForwardGram')
 
 # Constants
 api_id_key = 'api_id'
@@ -30,22 +36,20 @@ async def get_current_session_name():
 
 def extract_param(argv, index):
     if argv[index] is None or len(argv[index]) == 0:
-        sys.stderr.write('Error! Provide argument on {} position.'.format(index))
+        LOGGER.error('Error! Provide argument on %s position.', index)
         exit(1)
     return argv[index]
 
 
 def extract_params(argv, forward_from_index, forward_to_index):
     if len(argv) < 3:
-        sys.stderr.write(
-            'Error! Provide arguments "forward_from" as a first argument and "forward_to" as a second argument. You have provided {} argument(s)'.format(
-                len(argv) - 1))
+        LOGGER.error('Error! Provide arguments "forward_from" as a first argument and "forward_to" as a second argument. You have provided %s argument(s)', len(argv) - 1)
         exit(1)
     if argv[forward_from_index] is None or len(argv[forward_from_index]) == 0:
-        sys.stderr.write('Error! Provide argument "forward_from" as a first argument.')
+        LOGGER.error('Error! Provide argument "forward_from" as a first argument.')
         exit(1)
     if argv[forward_to_index] is None or len(argv[forward_to_index]) == 0:
-        sys.stderr.write('Error! Provide argument "forward_to" as a second argument.')
+        LOGGER.error('Error! Provide argument "forward_to" as a second argument.')
         exit(1)
     return [argv[forward_from_index], argv[forward_to_index]]
 
@@ -57,7 +61,7 @@ def resolve_sender_id(peer_id):
         return peer_id.chat_id
     if hasattr(peer_id, 'user_id'):
         return peer_id.user_id
-    print('Error! Cannot resolve sender_id by peer="{}"'.format(peer_id))
+    LOGGER.error('Error! Cannot resolve sender_id by peer="%s"', peer_id)
     return None
 
 
@@ -65,11 +69,11 @@ async def handle_new_message(event):
     sender_id = resolve_sender_id(event.message.peer_id)
 
     if sender_id is None:
-        print('Ignoring message "{}"'.format(event.message))
+        LOGGER.warn('Ignoring message "%s"', event.message)
         return
 
     if sender_id == source_dialog_id:
-        print('Sending message from "{}" to "{}".'.format(forward_from_name, forward_to_name))
+        LOGGER.info('Sending message from "%s" to "%s".', forward_from_name, forward_to_name)
         await client.send_message(target_dialog, event.message)
 
 
@@ -101,15 +105,14 @@ def start_forwarding():
     client.start()
 
     current_session = client.loop.run_until_complete(get_current_session_name())
-    print('Running as {}(@{}).'.format(current_session[0], current_session[1]))
+    LOGGER.info('Running as %s(@%s).', current_session[0], current_session[1])
 
     global target_dialog, source_dialog_id
     target_dialog = client.loop.run_until_complete(fetch_dialog(forward_to_name))
     source_dialog_id = client.loop.run_until_complete(fetch_dialog(forward_from_name)).entity.id
 
     client.add_event_handler(handle_new_message, events.NewMessage)
-    print('Forwarding from "{}"(id={}) to "{}"(id={})'.format(forward_from_name, source_dialog_id, forward_to_name,
-                                                              target_dialog.entity.id))
+    LOGGER.info('Forwarding from "%s"(id=%s) to "%s"(id=%s)', forward_from_name, source_dialog_id, forward_to_name, target_dialog.entity.id)
     client.run_until_disconnected()
 
 
@@ -118,7 +121,7 @@ def login():
     client = TelegramClient(name, api_id, api_hash)
     client.start()
     current_session = client.loop.run_until_complete(get_current_session_name())
-    print('Signed in as {}(@{}).'.format(current_session[0], current_session[1]))
+    LOGGER.info('Signed in as %s(@%s).', current_session[0], current_session[1])
 
 
 def read_api_configuration():
@@ -130,19 +133,23 @@ def read_api_configuration():
 
 
 def main():
-    read_api_configuration()
+    try:
+        read_api_configuration()
 
-    login_command = 'login'
-    forward_command = 'forward'
-    command = extract_param(sys.argv, 1)
-    if command == login_command:
-        login()
-        return 0
-    if command == forward_command:
-        start_forwarding()
-        return 0
+        login_command = 'login'
+        forward_command = 'forward'
+        command = extract_param(sys.argv, 1)
+        if command == login_command:
+            login()
+            return 0
+        if command == forward_command:
+            start_forwarding()
+            return 0
+    except Exception as exception:
+        LOGGER.error(exception)
+        raise exception
 
-    print('Error! Unknown command "{}", available commands: "{}", "{}".'.format(command, login_command, forward_command))
+    LOGGER.info('Error! Unknown command "%s", available commands: "%s", "%s".', command, login_command, forward_command)
 
 
 if __name__ == '__main__':
